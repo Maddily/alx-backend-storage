af33445ec12c36f8512a0d424f9057c75672c84e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-13 14:07:09 +0300

    Create a table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Create a table `users`
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL,
+    name varchar(255),
+    UNIQUE (email),
+    PRIMARY KEY (id)
+);
