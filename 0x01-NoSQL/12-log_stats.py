1b3e0d2284277063dc26682d892851371c2da937
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-17 13:36:32 +0300

    Prevent execution if module is imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,21 +4,21 @@ This script retrieves and displays statistics
 from a MongoDB collection of nginx logs.
 """
 
-import pymongo
+if __name__ == '__main__':
+    import pymongo
 
+    client = pymongo.MongoClient()
+    nginx_collection = client.logs.nginx
 
-client = pymongo.MongoClient()
-nginx_collection = client.logs.nginx
+    print(nginx_collection.count_documents({}), 'logs')
+    print('Methods:')
 
-print(nginx_collection.count_documents({}), 'logs')
-print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        print(f'\tmethod {method}:',
+              nginx_collection.count_documents({'method': method}))
 
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-for method in methods:
-    print(f'\tmethod {method}:',
-          nginx_collection.count_documents({'method': method}))
-
-print(nginx_collection.count_documents({
-    'method': 'GET',
-    'path': '/status'
-}), 'status check')
+    print(nginx_collection.count_documents({
+        'method': 'GET',
+        'path': '/status'
+    }), 'status check')
