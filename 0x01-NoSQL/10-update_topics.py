d01f9d3eb9002271f7adff320576781bab4ef478
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-17 12:54:26 +0300

    Update a field in a document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to update the topics field
+of a document in a MongoDB collection.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics field of a document in the MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+            The MongoDB collection to update.
+        name (str): The name of the document to update.
+        topics (list): The new list of topics to set.
+
+    Returns:
+        None
+
+    """
+
+    mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
