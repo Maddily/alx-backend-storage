193379982bd7e49646662e56ec75e9e012f44038
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-19 18:47:04 +0300

    Create a class that interacts with Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+This module contains a Cache class that
+interacts with Redis to store data.
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache():
+    """
+    Cache class for storing data in Redis.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes a new instance of the Cache class.
+        """
+
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str | bytes | int | float]) -> str:
+        """
+        Stores the given data in Redis and returns the generated key.
+
+        Args:
+            data: The data to be stored.
+                It can be a string, bytes, int, or float.
+
+        Returns:
+            The generated key used to store the data in Redis.
+        """
+
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
